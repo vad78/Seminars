686ee614ee7703569c9402c9c69daf56eacb1c9b
Vadim Yukin <yvadim@mail.ru>
2022-11-29 21:42:42 +0300

    init: new project seminar1

---
+++ Seminar1/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Enter first value ");
+int value1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enetr second value ");
+int value2 = Convert.ToInt32(Console.ReadLine());
+int pow1 = value1*value1;
+if (pow1==value2*value2)
+{
+    Console.WriteLine("Value2 is the square of "+value1);
+}
+else
+{
+    Console.WriteLine("Value2 is not the square of "+value1);
+}
