d6ba56d8dd0e2f86ddb61812031748f2b8b759c0
Vadim Yukin <yvadim@mail.ru>
2022-11-30 11:29:11 +0300

    add even numeric series

---
+++ Homework1/numberSeries/binSeries/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+Console.Write("Enter number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int check = num%2;
+if (check!=0) num=num-1;
+Console.Write("Even numeric series: ");
+for(int i=2;i<=num;i=i+2)
+{
+    Console.Write(i+" ");
+}
+ 
