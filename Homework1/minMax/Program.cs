c8b36f619feee7260e441a779eda0d42d68b0c0d
Vadim Yukin <yvadim@mail.ru>
2022-11-30 09:05:02 +0300

    add find min&max

---
+++ Homework1/minMax/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Enter first number ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter second number ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+int max = num1;
+if(max<num2)
+{
+    Console.WriteLine("Maximum is "+num2+" and minimum is "+num1);
+}
+else
+{
+    Console.WriteLine("Maximum is "+num1+" and minimum is "+num2);
+}
