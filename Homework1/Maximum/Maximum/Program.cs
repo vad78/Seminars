f3bbca8a9fc8c4a8741057062b84fc7b81bcd5ab
Vadim Yukin <yvadim@mail.ru>
2022-11-30 09:32:48 +0300

    add maximum search

---
+++ Homework1/Maximum/Maximum/Program.cs
@@ -0,0 +1,14 @@
+﻿int [] numbers = new int[3];
+for(int i=0;i<3;i++)
+{
+    int numindex=i+1;
+    Console.Write("Please enter num"+numindex+": ");
+    numbers[i] = Convert.ToInt32(Console.ReadLine());
+}
+int max=numbers[0];
+for(int i=0;i<3;i++)
+{
+    if (numbers[i]>max) max=numbers[i];
+}
+
+Console.WriteLine("Maximum is "+max);
