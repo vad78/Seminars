98d2ddc47f77891dfc22189150f1795632435c17
Vadim Yukin <yvadim@mail.ru>
2022-11-30 09:41:58 +0300

    add even numbers

---
+++ Homework1/evenNumber/evenNum/Program.cs
@@ -0,0 +1,11 @@
+﻿
+Console.Write("Enter number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if((number%2)==0)
+{
+    Console.WriteLine(number+" is even number");
+}
+else
+{
+    Console.WriteLine(number+" is not even number");
+}
