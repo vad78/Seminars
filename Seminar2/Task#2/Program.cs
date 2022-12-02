46d52ca54ef0cdb1c258fb8909b98f5ed91de8dc
Vadim Yukin <yvadim@mail.ru>
2022-12-02 20:54:17 +0300

    add: second task

---
+++ Seminar2/Task#2/Program.cs
@@ -0,0 +1,16 @@
+﻿/*Напишите программу, которая будет принимать на вход два числа и
+ выводить, является ли первое число кратным второму. Если число 2 не кратно числу 1, 
+ то программа выводит остаток от деления. 34, 5 -> не кратно, остаток 4 16, 4 -> кратно*/
+Console.Write("Enter first num");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter second num");
+ int num2 = Convert.ToInt32(Console.ReadLine());
+ if(num1%num2==0) 
+ {
+    Console.WriteLine("First num is multiple ");
+ }
+else
+{
+    Console.WriteLine("First num is not multiple ");
+}
+
