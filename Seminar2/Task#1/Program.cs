e98bc4fb5b49e0c1ab9b65259da3514e73727293
Vadim Yukin <yvadim@mail.ru>
2022-12-02 20:36:43 +0300

    add: first task

---
+++ Seminar2/Task#1/Program.cs
@@ -0,0 +1,6 @@
+﻿//Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа. 456 -> 46 782 -> 72 918 -> 98
+int num = new Random().Next(100, 1000);
+int lastDigit = num%10;
+int firstDigit = num/100;
+Console.WriteLine($"Initial num is {num} {firstDigit} {lastDigit}");
+
