7de67bb2d1b0c15e6e45f482e2cdfcc418376af4
Vadim Yukin <yvadim@mail.ru>
2022-12-09 21:09:15 +0300

    add: task array

---
+++ Seminar4/Task#3/Program.cs
@@ -0,0 +1,17 @@
+﻿int [] SetArray()
+{
+    int [] myArray = new int[8];
+    for(int i =0;i<8;i++)
+    {
+       
+        myArray[i] = new Random().Next(0, 2);
+    }
+return myArray;
+}
+void ArrayOutput(int [] arr)
+{
+    for(int i =0;i<8;i++)
+    Console.Write($"{arr[i]} ");
+}
+int [] myArray = SetArray();
+ArrayOutput(myArray);
