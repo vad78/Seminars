53e55c44c6519b523ff78e8fc5331f918a8626e3
Vadim Yukin <yvadim@mail.ru>
2022-12-14 18:48:36 +0300

    add: task 2 calculation of non eveve members

--- Homework5/Task#2/Program.cs
+++ Homework5/Task#2/Program.cs
@@ -7,6 +7,10 @@ class Program
     {
         Input MyNum = new Input();
         int number = MyNum.Num();
+        SummArrayNum myEvenSumm = new SummArrayNum(number);
+        myEvenSumm.Print();
+        int summ = myEvenSumm.SummNonEvenMembers();
+        Console.WriteLine($"The sum of non even array members is {summ}");
 
     }
     class Input
@@ -28,5 +32,48 @@ class Program
              
         }
     }
-     
+     class SummArrayNum
+     {
+        private int arraySize;
+        private int[] array;
+        private int count;
+        public SummArrayNum(int size)
+        {
+            this.arraySize = size;
+            int [] arr = new int[arraySize];
+            Random myRandom = new Random();
+            for(int i=0;i<arr.Length;i++)
+            {
+                arr[i] = myRandom.Next(1, 1000);
+            }
+            this.array = arr;
+        }
+        public void Print()
+        {
+            PrintArray();
+        }
+        public int[]  GetArray()
+        {
+            return this.array;
+        }
+        public int SummNonEvenMembers()
+        {
+            int sum = CalcNonEvenMembers();
+            return sum;
+        }
+        
+        private int CalcNonEvenMembers()
+        {
+             int summ = 0;
+            for(int i = 1; i<this.array.Length;i+=2)
+            {
+                summ=summ+this.array[i];
+            }
+            return summ;
+        }
+         private void PrintArray()
+        {
+            Console.WriteLine("Array: "+string.Join(", ", this.array));
+        }
+     }
 }
