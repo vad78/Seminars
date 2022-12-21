fdede58e6bd8924b18cbacc5b231062556bb5c61
Vadim Yukin <yvadim@mail.ru>
2022-12-21 09:42:11 +0300

    add: task1

---
+++ Seminar7/Task#1/Program.cs
@@ -0,0 +1,41 @@
+﻿/*Задача 48: Задайте двумерный массив размера m на n, каждый элемент в массиве находится по формуле: Ai,j = i+j. 
+Выведите полученный массив на экран.
+m = 3, n = 4. 0 1 23
+1 2 34
+2 3 45*/
+class Program
+{
+    public static void Main(string[] args)
+    {
+       MyMatrix matrix1 =new MyMatrix(2,2);
+       matrix1.PrintArray();
+    }
+class MyMatrix
+{
+    //private double matrixRowCount, matrixString;
+    private int [,] matrixArray;
+    public MyMatrix(int numForMatrixString, int NumForMatrixRow)
+    {
+       int [,] arr= new int[numForMatrixString, NumForMatrixRow];
+        for(int i = 0; i<arr.GetLength(0); i++)
+        {
+            for(int j = 0; j<arr.GetLength(1); j++)
+            {
+                arr[i,j] = i+j;
+            }
+        }
+        this.matrixArray = arr;
+    }
+    public void PrintArray()
+    {
+       
+        for(int i = 0; i<this.matrixArray.GetLength(0); i++)
+        {
+            for(int j = 0; j<this.matrixArray.GetLength(1); j++)
+            {
+                Console.WriteLine(this.matrixArray[i,j]);
+            }
+        }
+    }
+ }
+}
