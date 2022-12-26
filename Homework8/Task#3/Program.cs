6f273c448222cdc68958b6cd994011f37ce415d2
Vadim Yukin <yvadim@mail.ru>
2022-12-26 17:04:23 +0300

    add: task3 empty multiply private method

---
+++ Homework8/Task#3/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Task3
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Input myMatrixSize = new Input();
+            int rowCount = myMatrixSize.IndexOfElement(1);
+            int cellCount = myMatrixSize.IndexOfElement(2);
+            MyIntMatrixArray myArray = new MyIntMatrixArray();
+            myArray.FillArray(rowCount, cellCount);
+            int cellCountForArray2 = myMatrixSize.IndexOfElement(2);
+            myArray.FillArray2(cellCountForArray2);
+            myArray.PrintArrays();
+        }
+    }
+}
