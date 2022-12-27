8430ac5483e8bc4f1f75024b4d6c2241e3f9c3b9
Vadim Yukin <yvadim@mail.ru>
2022-12-27 16:37:37 +0300

    del: previos boolshit

--- Homework8/Task#5/MyIntMatrixArray.cs
+++ Homework8/Task#5/MyIntMatrixArray.cs
@@ -3,10 +3,11 @@ namespace Task5
     class MyIntMatrixArray
     {
         private int[,] myArray;
+        private int size;
         public MyIntMatrixArray(int sizeOfMatrix)
         {
-            
             this.myArray = new int[sizeOfMatrix,sizeOfMatrix];
+            
         }
         public void Print()
         {
@@ -31,43 +32,11 @@ namespace Task5
         
         private void FillArray()
         {
-            int x=0,
-                y=0,
-                count = 1;
-            y = MoveLeft(y, x, count).Item1;
-            x = MoveLeft(y, x, count).Item2;
-            count =  MoveLeft(y, x, count).Item3;
-            y = MoveDown(y, x, count).Item1;
-            x = MoveDown(y, x, count).Item2;
-            count =  MoveDown(y, x, count).Item3; 
-                
-                
-                 
+            
+            
+            
         }
-        private (int,int,int) MoveLeft(int y, int x, int count)
-        {
-            for(int i = 0;i<this.myArray.GetLength(0);i++)
-                {
-                    x = i;
-                    this.myArray[y,x] = count;
-                    count++;
-                }
-                return (x,y,count);
-                
-        }
-        private (int,int,int) MoveDown(int y, int x, int count)
-        {
-           
-           
-            for(int i = 1 ;i<this.myArray.GetLength(1);i++)
-                {
-                    y=i;
-                    this.myArray[y,x] = count;
-                    count++;
-                }
-            return (x,y,count);
-                
-        }
+        
     }    
        
 }
