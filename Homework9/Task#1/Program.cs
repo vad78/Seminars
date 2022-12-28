484ce2f5b1c65c4c4fddebf1bb865ce3f41fc71f
Vadim <yvadim@mail.ru>
2022-12-28 21:52:17 +0300

    add: task1

---
+++ Homework9/Task#1/Program.cs
@@ -0,0 +1,28 @@
+﻿/*Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+N = 5 -> "5, 4, 3, 2, 1"
+N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"*/
+namespace Homework9Task1
+{
+    class Program
+    {
+        public static void Main()
+        {
+            void serialOfNumbers(int Number)
+            {
+            //Console.WriteLine();
+                if(Number == 0)
+                {
+                    return;
+                }
+                else
+                {
+                    Console.Write($"{Number} ");
+                    serialOfNumbers(Number-1);
+                }                  
+            }
+            Input myNum = new Input();
+            int number = myNum.InputNumber();
+            serialOfNumbers(number);    
+        }
+    }
+}
