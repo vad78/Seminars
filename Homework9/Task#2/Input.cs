6019313791b1fdd7bfd5b54c80b26498ad68303b
Vadim <yvadim@mail.ru>
2022-12-28 23:17:42 +0300

    add: task2

---
+++ Homework9/Task#2/Input.cs
@@ -0,0 +1,24 @@
+namespace Homework9Task2
+{
+    class Input
+    {
+        private int number;
+        public int InputNumber()
+        {
+            this.number = Number();
+            return this.number;
+        }
+       
+        private int Number()
+        {
+            int num = 0;
+            Console.Write("Enter pozitive integer number: ");
+            while((Int32.TryParse(Console.ReadLine(), out num)==false)||num<=0)
+            {
+                Console.Write("Enter correct number: ");
+            }
+            return num;
+
+        }    
+    }
+}
---
+++ Homework9/Task#2/Program.cs
@@ -0,0 +1,42 @@
+﻿/*
+Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N. Выполнить с помощью рекурсии.
+
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+*/
+namespace Homework9Task2
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Input myNum = new Input();
+            int number1 = myNum.InputNumber();
+            int number2 = myNum.InputNumber();
+            if(number2<=number1)
+            {
+                Console.WriteLine($"Enter num greater than {number1}");
+                number2 = myNum.InputNumber();
+            }
+            int summary = SummOfNum(number1, number2);
+            Console.WriteLine($"The summ of numbers from {number1} to {number2} is {summary}");
+            int SummOfNum(int startNum, int endNum)
+            {
+                             
+                int summ = 0;
+                if(startNum==endNum)
+                {
+                   
+                    summ = startNum;
+                }
+                else
+                {
+                    summ = startNum+SummOfNum(startNum+1,endNum);    
+                }
+                return summ;
+                
+            }
+        }
+    }
+}
+        
